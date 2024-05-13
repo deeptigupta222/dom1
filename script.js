b03219938b11b3fd3f29eeaa2cabb3ddd80be6ad
deeptigupta222 <deeptig22@navgurukul.org>
2024-05-13 19:49:13 +0530

    dom commited

---
+++ script.js
@@ -0,0 +1,36 @@
+console.dir(document.body)
+console.log(document.body)
+console.log(document.head)
+
+
+let x=document.getElementById("heading")
+console.dir(x)
+
+let y=document.getElementsByClassName("heading-1")
+console.log(y)
+
+
+let z=document.getElementsByTagName("p")
+console.log(z)
+
+
+let fisrt_elements=document.querySelector("p") //first element
+console.log(fisrt_elements)
+
+
+let all=document.querySelectorAll("p")  //all elements
+console.log(all)
+
+
+
+let first_class=document.querySelector(".heading-1")
+console.log(first_class)
+
+let all_class=document.querySelectorAll(".heading-1")
+console.log(all_class)
+
+
+
+
+let div=document.querySelector("div")
+console.dir(div)
